cd3a4e27599a0a9af3395bf54ba93a16d7c0e7d7
Antek Cichon <antekcichon@gmail.com>
2026-04-17 09:54:16 +0200

    add setup call

--- __main__.py
+++ __main__.py
@@ -2,19 +2,15 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 from src.util.config import get_config, get_timer
-from src.util.logging import LogMessage, TerminalMessage
-from src.util.export import export_logs
-from src.util.static import WELCOME_ART
+from src.util.setup import run_setup
 
 def main():
-    pass
+    print("MAC & SEIZE entry point")
 
 if __name__ == "__main__":
+    if not run_setup():
+        print("Setup failed, exiting.")
+        exit(1)
     global config, timer
     config = get_config()
     timer = get_timer()
-    TerminalMessage(WELCOME_ART, include_stamp = False, color = "yellow")
-    LogMessage(f"Application started, configuration loaded:\n{config.dump()}")
-    main()
-    export_logs(remove_original = config.logging.remove_on_exit)
-    exit(0)
---
+++ src/util/setup.py
@@ -0,0 +1,5 @@
+from src.util.static import WELCOME_ART
+
+def run_setup():
+    print(WELCOME_ART)
+    return True
