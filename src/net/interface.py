91e79d1d262a2719902e14ca05b0f12d4a9c596b
AntosCichon <antekcichon@gmail.com>
2026-04-21 12:25:41 +0200

    Interface class, able to read address

---
+++ src/net/interface.py
@@ -0,0 +1,63 @@
+import netifaces as ni
+
+def get_interfaces():
+    return ni.interfaces()
+
+class Interface:
+    def __init__(self, name: str):
+        if name not in get_interfaces():
+            raise ValueError(f"Interface '{name}' does not exist.")
+        self.name = name
+        self.ipv4 = {
+            "addr": list(),
+            "netmask": list(),
+            "broadcast": list(),
+            "peer": list(),
+            "count": 0
+        }
+        self.ipv6 = {
+            "addr": list(),
+            "netmask": list(),
+            "broadcast": list(),
+            "peer": list(),
+            "count": 0
+        }
+        self.mac = {
+            "addr": list(),
+            "peer": list(),
+            "count": 0
+        }
+
+        self.get_ipv4()
+        self.get_ipv6()
+        self.get_mac()
+
+    def get_ipv4(self, address_type = None) -> list | dict:
+        if ni.AF_INET not in ni.ifaddresses(self.name):
+            self.ipv4["count"] = 0
+            return self.ipv4[address_type] if address_type is not None else self.ipv4
+        info = ni.ifaddresses(self.name)[ni.AF_INET]
+        self.ipv4["count"] = len(info)
+        for address in [ address_type ] if address_type is not None else [ "addr", "netmask", "broadcast", "peer" ]:
+            self.ipv4[address] = [ info[i][address] if address in info[i] else None for i in range(self.ipv4["count"]) ]
+        return self.ipv4[address_type] if address_type is not None else self.ipv4
+    
+    def get_ipv6(self, address_type = None) -> list | dict:
+        if ni.AF_INET6 not in ni.ifaddresses(self.name):
+            self.ipv6["count"] = 0
+            return self.ipv6[address_type] if address_type is not None else self.ipv6
+        info = ni.ifaddresses(self.name)[ni.AF_INET6]
+        self.ipv6["count"] = len(info)
+        for address in [ address_type ] if address_type is not None else [ "addr", "netmask", "broadcast", "peer" ]:
+            self.ipv6[address] = [ info[i][address] if address in info[i] else None for i in range(self.ipv6["count"]) ]
+        return self.ipv6[address_type] if address_type is not None else self.ipv6
+    
+    def get_mac(self, address_type = None) -> list | dict:
+        if ni.AF_LINK not in ni.ifaddresses(self.name):
+            self.mac["count"] = 0
+            return self.mac[address_type] if address_type is not None else self.mac
+        info = ni.ifaddresses(self.name)[ni.AF_LINK]
+        self.mac["count"] = len(info)
+        for address in [ address_type ] if address_type is not None else [ "addr", "peer" ]:
+            self.mac[address] = [ info[i][address] if address in info[i] else None for i in range(self.mac["count"]) ]
+        return self.mac[address_type] if address_type is not None else self.mac
